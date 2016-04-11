006ae8729e0802b7d9683a1fd9ab196431213d9e
Geisson Machado Antunes <geisonnm@hotmail.com>
2016-04-11 20:57:40 -0300

    config helpers

---
+++ Helpers/Url.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace geissonmachado\Helpers;
+
+
+
+abstract class Url {
+	
+	
+	public function callHello()
+	{
+		echo "Hello Baby i am URL";
+	}
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once "vendor/autoload.php";
+
+use geissonmachado\Helpers\Url;
+
+Url::callHello();
